7c70b6e2596172caec50ec8ee3855ec5de7590ff
junseok0304 <83647215+junseok0304@users.noreply.github.com>
2021-06-12 22:33:32 +0900

    Create 01variable.kt

---
+++ 01variable.kt
@@ -0,0 +1,22 @@
+package Kotlin
+
+var num = 10
+var hello ="hello"
+var point = 3.4
+
+val fix = 20
+
+fun main(args:Array<String>){
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+
+    num=100
+    hello="good bye"
+    point=10.4
+    println(num)
+    println(hello)
+    println(point)
+
+}
