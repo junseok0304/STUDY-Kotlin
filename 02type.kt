3b58b9ff09aca83f0d017a5461dbca0ad5fdeddd
junseok0304 <83647215+junseok0304@users.noreply.github.com>
2021-06-13 18:06:35 +0900

    Create 02type.kt

---
+++ 02type.kt
@@ -0,0 +1,29 @@
+  package com.example.test001
+
+//02.자료형
+// 정수형 -> Long > Int > Short > Byte
+// 실수형 -> Double > Float
+// 문자 -> Char = 가, 나
+// 문자열 -> String = 여러 자 abcd, 안녕하세요 등
+// 논리형 참/거짓 -> Boolean
+
+var number1 : Int = 20
+var hello1: String = "Hello"
+var point1 : Double = 3.4
+
+// variable or value??
+// 1. 변하지 않는 값이라면 Value
+// 2. 변할지 안변할지 모르겠는 값이라면 Variable
+
+
+fun main(array: Array<String>){
+    print(number1)
+    print(hello1)
+    print(point1)
+
+}
+
+
+
+=> 20Hello3.4
+Process finished with exit code 0
